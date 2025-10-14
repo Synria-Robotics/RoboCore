00efbc4f6d7c9c51f7b1393ff16e745f3efab691
@pay-the-rain <1840811549@qq.com>
2025-10-14 16:06:24 +0800

    Add lacking file

--- robocore/kinematics/__init__.py
+++ robocore/kinematics/__init__.py
@@ -24,14 +24,6 @@ from .ik import inverse_kinematics  # unified IK
 from .jacobian import jacobian  # unified Jacobian
 from .bimanual import bimanual_forward_kinematics, bimanual_inverse_kinematics, bimanual_jacobian
 
-# Solver classes (not exported, for advanced usage)
-from .fk_utils.fk_solver_numpy import FKSolverNumPy
-from .fk_utils.fk_solver_torch import FKSolverTorch  # type: ignore
-from .ik_utils.ik_solver_numpy import IKSolverNumPy
-from .ik_utils.ik_solver_torch import IKSolverTorch  # type: ignore
-from .jacobian_utils.jacobian_solver_numpy import JacobianSolverNumPy
-from .jacobian_utils.jacobian_solver_torch import JacobianSolverTorch  # type: ignore
-
 __all__ = [
     # unified API (main public interface)
     "forward_kinematics",
